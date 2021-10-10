802b9a5722abb39c0fa11ca86e2157479447be15
KirSaxhlil <75578152+KirSaxhlil@users.noreply.github.com>
2021-10-10 19:09:03 +0300

    Create Lab5.cs

---
+++ Lab5.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int DivideString(String line, char symb)
+        {
+            if (line.IndexOf(symb) != -1)
+                return line.IndexOf(symb);
+            else return 0;
+        }
+
+        static int DivideStringReg(String line, char symb)
+        {
+            Regex reg = new Regex(@"" + symb);
+            return reg.Match(line).Index;
+        }
+
+        static void Main(string[] args)
+        {
+            string line;
+            char a;
+            line = Console.ReadLine();
+            a = Console.ReadKey().KeyChar;
+            Console.Write("\n");
+            Console.WriteLine("By String: " + DivideString(line, a));
+            Console.WriteLine("By Regex: " + DivideStringReg(line, a));
+            Console.ReadKey();
+        }
+    }
+}
