6f3bd1517a34a8c4c08f6be0909dd6f4426d8f2f
KirSaxhlil <75578152+KirSaxhlil@users.noreply.github.com>
2021-12-14 21:51:59 +0300

    Update Lab12.cs

--- Lab12.cs
+++ Lab12.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace Lab12
 {
-    class ProgLib
+    public class ProgLib
     {
-        List<AppliedProg> list = new List<AppliedProg>();
+        public List<AppliedProg> list = new List<AppliedProg>();
 
         public void Add(AppliedProg prog)
         {
             list.Add(prog);
         }
-        public void Edit(int n)
+        public void Edit(int n, int field, string value)
         {
-            list[n].Edit();
+            list[n].Edit(field, value);
         }
         public void Delete(int n)
         {
@@ -24,39 +24,46 @@ namespace Lab12
         }
         public void Output()
         {
-            for(int i = 0; i < list.Count; i++)
+            for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine("{0} {1} {2} {3} {4}", list[i].name, list[i].code_size, list[i].lang, list[i].point, list[i].user_category);
             }
         }
     }
 
-    abstract class CompProg
+    public abstract class CompProg
     {
         public string name;
         public int code_size;
         public string lang;
 
-        public abstract void Edit();
+        public abstract void Edit(int field, string value);
     }
 
-    class AppliedProg : CompProg
+    public class AppliedProg : CompProg
     {
         public string point;
         public string user_category;
 
-        public override void Edit()
+        public AppliedProg() { }
+        public AppliedProg(string name, int code_size, string lang, string point, string user_category)
         {
-            Console.WriteLine("Выберите изменяемое поле:\n1) Название\n2) Объем кода\n3) Язык программирования\n4) Назначение\n5) Категория пользователей");
-            int choice = int.Parse(Console.ReadKey().KeyChar.ToString());
-            Console.WriteLine();
-            switch(choice)
+            this.name = name;
+            this.code_size = code_size;
+            this.lang = lang;
+            this.point = point;
+            this.user_category = user_category;
+        }
+
+        public override void Edit(int field, string value)
+        {
+            switch (field)
             {
-                case 1: name = Console.ReadLine(); break;
-                case 2: code_size = int.Parse(Console.ReadLine()); break;
-                case 3: lang = Console.ReadLine(); break;
-                case 4: point = Console.ReadLine(); break;
-                case 5: user_category = Console.ReadLine(); break;
+                case 1: name = value; break;
+                case 2: code_size = int.Parse(value); break;
+                case 3: lang = value; break;
+                case 4: point = value; break;
+                case 5: user_category = value; break;
             }
         }
     }
@@ -85,33 +92,52 @@ namespace Lab12
                 }
                 switch (choice)
                 {
-                    case 1: {
+                    case 1:
+                        {
+                            Console.Clear();
                             AppliedProg prog = new AppliedProg();
                             try
                             {
+                                Console.Write("Название: ");
                                 prog.name = Console.ReadLine();
+                                Console.Write("Объем кода: ");
                                 prog.code_size = int.Parse(Console.ReadLine());
+                                Console.Write("Язык программирования: ");
                                 prog.lang = Console.ReadLine();
+                                Console.Write("Назначение: ");
                                 prog.point = Console.ReadLine();
+                                Console.Write("Категория пользователей: ");
                                 prog.user_category = Console.ReadLine();
                                 list.Add(prog);
                             }
-                            catch(FormatException ex) { Console.WriteLine(ex.Message); }
+                            catch (FormatException ex) { Console.WriteLine(ex.Message); Console.ReadKey(); }
                             break;
                         }
                     case 2:
                         {
-                            try { list.Edit(int.Parse(Console.ReadLine())); }
-                            catch (FormatException ex) { Console.WriteLine(ex.Message); }
+                            Console.Clear();
+                            try
+                            {
+                                Console.WriteLine("Введите номер элемента: ");
+                                int n = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Выберите изменяемое поле:\n1) Название\n2) Объем кода\n3) Язык программирования\n4) Назначение\n5) Категория пользователей");
+                                int field = int.Parse(Console.ReadKey().KeyChar.ToString());
+                                Console.WriteLine("Введите новое значение: ");
+                                string value = Console.ReadLine();
+                                list.Edit(n, field, value);
+                            }
+                            catch (Exception ex) { Console.WriteLine(ex.Message); Console.ReadKey(); }
                             break;
                         }
                     case 3:
                         {
+                            Console.Clear();
+                            Console.WriteLine("Введите номер элемента: ");
                             try { list.Delete(int.Parse(Console.ReadLine())); }
-                            catch (FormatException ex) { Console.WriteLine(ex.Message); }
+                            catch (Exception ex) { Console.WriteLine(ex.Message); Console.ReadKey(); }
                             break;
                         }
-                    case 4: list.Output(); break;
+                    case 4: Console.Clear(); list.Output(); Console.ReadKey(); break;
                     case 5: Quit(ref work); break;
                 }
             } while (work);
